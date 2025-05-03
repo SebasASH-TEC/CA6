bf7d2bbc81ea806f279b98dd20f61aeeea35199a
Sebastián Solís <s.solis.3@estudiantec.cr>
2025-05-02 21:29:08 -0600

    Add project files.

---
+++ CA6.Tests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Algorithms;
+
+namespace CA6.Tests
+{
+    public class BinarySearchTests
+    {
+        [Fact]
+        public void Search_ReturnsCorrectIndex_WhenElementIsFound()
+        {
+            int[] sortedArray = { 1, 3, 5, 7, 9, 11 };
+            int result = BinarySearch.Search(sortedArray, 7);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Search_ReturnsMinusOne_WhenElementIsNotFound()
+        {
+            int[] sortedArray = { 2, 4, 6, 8, 10 };
+            int result = BinarySearch.Search(sortedArray, 5);
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void Search_ReturnsMinusOne_WhenArrayIsEmpty()
+        {
+            int[] emptyArray = { };
+            int result = BinarySearch.Search(emptyArray, 1);
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void Search_ReturnsCorrectIndex_WhenElementIsAtBeginning()
+        {
+            int[] sortedArray = { 1, 2, 3, 4, 5 };
+            int result = BinarySearch.Search(sortedArray, 1);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Search_ReturnsCorrectIndex_WhenElementIsAtEnd()
+        {
+            int[] sortedArray = { 1, 2, 3, 4, 5 };
+            int result = BinarySearch.Search(sortedArray, 5);
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class BinarySearch
+    {
+        /// <summary>
+        /// Performs binary search on a sorted array.
+        /// </summary>
+        /// <param name="array">Sorted integer array</param>
+        /// <param name="target">Value to search for</param>
+        /// <returns>Index of target if found; otherwise, -1</returns>
+        public static int Search(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target)
+                    return mid;
+                else if (array[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return -1; // Not found
+        }
+    }
+}
