8665ac055d22fa823f5938ee82a90a2ce062647a
Sebastián Solís <s.solis.3@estudiantec.cr>
2025-05-03 23:52:30 -0600

    [ADD] LinearSearch

---
+++ CA6.Tests/Class1.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms;
+using Xunit;
+
+namespace CA6.Tests
+{
+    public class LinearSearchTests
+    {
+        [Fact]
+        public void ElementFound_ReturnsCorrectIndex()
+        {
+            int[] arr = { 5, 10, 15, 20 };//arreglo
+            int result = LinealSearch.LinearSearch(arr, 15);//lo que se quiere buscar 
+            Assert.Equal(2, result); //en este caso si  encuentra el resultado 
+        }
+
+        [Fact]
+        public void ElementNotFound_ReturnsMinusOne()
+        {
+            int[] arr = { 1, 2, 3, 4 };//arreglo
+            int result = LinealSearch.LinearSearch(arr, 10);//lo que se quiere buscar en el arreglo anterior
+            Assert.Equal(-1, result);// en este caso no se encuentra el elemento en el arreglo
+        }
+
+        [Fact]
+        public void EmptyArray_ReturnsMinusOne()
+        {
+            int[] arr = { };//arreglo vacio
+            int result = LinealSearch.LinearSearch(arr, 1);//lo que se quiere buscar
+            Assert.Equal(-1, result);//no se encuentra pues el arreglo esta vacio
+        }
+
+        [Fact]
+        public void DuplicatedElement_ReturnsFirstIndex()
+        {
+            int[] arr = { 3, 5, 3, 7, 3 };//arreglo
+            int result = LinealSearch.LinearSearch(arr, 3);//lo que se busca
+            Assert.Equal(0, result);// da como resultado 0, pues el primer elemento que encontro esta en esa posicion a pesar de que se encuantra repetido
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{ //Clase para la busqueda lineal
+    public static class LinealSearch
+    {
+        public static int LinearSearch(int[] arr, int target) //Obtiene los datos, el array y el elemento a buscar
+        {
+            for (int i = 0; i < arr.Length; i++) //recorre todo el array elemento por elemento
+            {
+                if (arr[i] == target) //compara el elemento del array con el elemento a buscar
+                {
+                    return i; //retorna el elemento en caso de que este se encuentre
+                }
+            }
+            return -1; //retorna -1 en caso de haber recorrido todo el array y no encontrar el elemento
+        }
+    }
+}
